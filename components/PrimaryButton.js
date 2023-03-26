cf8cd8c0aa8e7960bb57fd3867aac16dfe63cd56
kosi2109slmm <sithuhtet@securelinkmm.com>
2023-03-26 12:42:47 +0630

    finish UI

---
+++ components/PrimaryButton.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Pressable, Text, View, StyleSheet } from "react-native";
+
+function PrimaryButton({ children }) {
+  const onPressHandler = () => {
+    console.log("click");
+  };
+  return (
+    <View style={styles.outerContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.innerContainer, styles.pressed]
+            : styles.innerContainer
+        }
+        onPress={onPressHandler}
+        android_ripple={{ color: "#640233" }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  outerContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  innerContainer: {
+    backgroundColor: "#72063c",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  //for ios
+  pressed: {
+    opacity: 0.75,
+  },
+});
---
+++ screens/GameScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+function GameScreen() {
+  return (
+    <Text>GameScreen</Text>
+  )
+}
+
+export default GameScreen
